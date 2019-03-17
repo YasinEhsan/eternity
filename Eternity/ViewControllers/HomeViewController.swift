7c22a49c128234e0789b2875a057efe1a6851660
user914637 <user914637@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-03-17 04:59:34 -0400

    t Before graph works

--- Eternity/ViewControllers/HomeViewController.swift
+++ Eternity/ViewControllers/HomeViewController.swift
@@ -7,24 +7,39 @@
 //
 
 import UIKit
+import Charts
 
-class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var score: UILabel!
+    var avg = Double(0)
+    
+    @IBAction func randomize(_ sender: UIButton) {
+        //let count = Int(arc4random_uniform(20)+1)
+        setChartValues(30)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setChartValues()
     }
-    */
-
+    
+    func setChartValues(_ count : Int = 30) {
+        let values = (0..<count).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(UInt32(84)) + 168) //From 210 +/- 42
+            avg = avg + val
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        avg = avg/30
+        avg.round(.down)
+        
+        score.text = String(avg);
+        let set1 = LineChartDataSet(values: values, label: "Daily Carbon Footprint (lbs)")
+        let data = LineChartData(dataSet: set1)
+        
+        self.lineChartView.data = data
+        
+    }
+    
 }
