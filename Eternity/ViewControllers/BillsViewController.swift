8cb4f92dc93aaf9d05a7f36ba909ce0a9a033b94
Yasinehsan <yasinehsan11@gmail.com>
2019-03-16 23:35:47 -0400

    added custom VCs

---
+++ Eternity/ViewControllers/BillsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BillsViewController.swift
+//  Eternity
+//
+//  Created by A. Rodgers on 3/16/19.
+//  Copyright © 2019 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class BillsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
