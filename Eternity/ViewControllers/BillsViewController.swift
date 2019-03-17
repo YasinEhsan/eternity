874038d57b13570c47b69a1f4d1ec90b085f6950
hchowdhury3273 <hc2324@nyu.edu>
2019-03-17 11:40:00 -0400

    Taking images to database

--- Eternity/ViewControllers/BillsViewController.swift
+++ Eternity/ViewControllers/BillsViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
-class BillsViewController:UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BillsViewController:UIViewController, UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var myImg: UIImageView!
     let data = ["$98.45", "$119.90", "$110.78", "$130.87", "$135.34", "$156.76"]
     
     let pics = ["bill1", "bill2", "bill3", "bill4", "bill5", "bill6"]
@@ -49,5 +51,85 @@ class BillsViewController:UIViewController, UITableViewDataSource, UITableViewDe
         // Pass the selected object to the new view controller.
     }
     */
-        
+    
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = UIImagePickerController.InfoKey.originalImage as? UIImage {
+            myImg.image = pickedImage
+            let imageName = NSUUID().uuidString
+            
+            let storageRef = Storage.storage().reference().child("profile_images").child("\(imageName).png")
+            
+            
+            
+            if let uploadData = self.provideImageData.myImge.image!.pngData() {
+                
+                
+                
+                storageRef.putData(uploadData, metadata: nil, completion: { (_, err) in
+                    
+                    
+                    
+                    if let error = error {
+                        
+                        print(error)
+                        
+                        return
+                        
+                    }
+                    
+                    
+                    
+                    storageRef.downloadURL(completion: { (url, err) in
+                        
+                        if let err = err {
+                            
+                            print(err)
+                            
+                            return
+                            
+                        }
+                        
+                        
+                        
+                        guard let url = url else { return }
+                        
+                        let values = ["name": name, "email": email, "profileImageUrl": url.absoluteString]
+                        
+                        
+                        
+                        self.registerUserIntoDatabaseWithUID(uid, values: values as [String : AnyObject])
+                        
+                    })
+                    
+                    
+                    
+                })
+                
+            }
+            
+            picker.dismiss(animated: true, completion: nil)
+            }
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
